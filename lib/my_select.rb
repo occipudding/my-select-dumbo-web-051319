34836d0733d561167afca7abd5a2daa6d9409098
Joshua Suskin <jsuskin85@gmail.com>
2019-05-07 03:36:02 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,11 @@
 def my_select(collection)
- i = 0
- while i < collection.length
-   
- end
+  i = 0
+  new_arr = []
+  while i < collection.length
+    if yield(collection[i])
+      new_arr << collection[i]
+    end
+    i += 1
+  end
+  new_arr
 end
